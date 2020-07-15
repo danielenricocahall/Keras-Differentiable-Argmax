02c9c369adc1381b2309c7ceab0b9b11bec3db53
danielenricocahall <danielenricocahall@gmail.com>
2020-07-14 20:52:47 -0400

    pydocs

--- loss/differentiable_argmax_approx.py
+++ loss/differentiable_argmax_approx.py
@@ -4,9 +4,9 @@ import tensorflow as tf
 def differentiable_argmax_approx(x, beta=1e2):
     """
     Approximation of argmax - translates to y = sum( i * exp(beta * x[i]) ) / sum( exp(beta * x[i]) )
-    :param x:
-    :param beta:
-    :return:
+    :param x: input data
+    :param beta: parameter which influences how accurate the approximation will be
+    :return: approximation of index with the maximum argument in x
     """
     y = tf.reduce_sum(tf.cumsum(tf.ones_like(x)) * tf.exp(beta * x) / tf.reduce_sum(tf.exp(beta * x))) - 1
     return y
