3a06804bdc2c4f417aaff2629d30e1cc0d8fd951
danielenricocahall <danielenricocahall@gmail.com>
2021-01-31 22:40:12 -0500

    updaate for tf 2.0

--- test/test_argmax_approx.py
+++ test/test_argmax_approx.py
@@ -1,28 +1,17 @@
-import pytest
-import tensorflow as tf
 import numpy as np
 
 from loss.differentiable_argmax_approx import differentiable_argmax_approx
 
 
-@pytest.fixture
-def session():
-    session = tf.Session()
-    yield session
-    session.close()
-
-
-def test_argmax_approx_single_max(session):
-    x = tf.placeholder(dtype=tf.float64, shape=(None,))
-    beta = 100
+def test_argmax_approx_single_max():
+    beta = 10
+    x = np.array([1, 2, 3, 10, 4, 5], dtype=np.float)
     y = differentiable_argmax_approx(x, beta)
-    data = np.array([1, 2, 3, 10, 4, 5])
-    assert data.argmax() == int(session.run(y, feed_dict={x: data / np.linalg.norm(data)}))
+    assert x.argmax() == y
 
 
-def test_argmax_approx_multiple_max(session):
-    x = tf.placeholder(dtype=tf.float64, shape=(None,))
-    beta = 100
+def test_argmax_approx_multiple_max():
+    beta = 10
+    x = np.array([1, 2, 10, 3, 10], dtype=np.float)
     y = differentiable_argmax_approx(x, beta)
-    data = np.array([1, 2, 10, 3, 10])
-    assert int(session.run(y, feed_dict={x: data / np.linalg.norm(data)})) == 3
+    assert y == 3
